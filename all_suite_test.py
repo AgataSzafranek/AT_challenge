48db20ccc765883799ce93d36e6642fecdd6f79e
Agata Szafranek <142822653+AgataSzafranek@users.noreply.github.com>
2023-12-11 11:32:51 -0600

    all_suite_test

--- all_suite_test.py
+++ all_suite_test.py
@@ -20,7 +20,6 @@ def full_suite():
     test_suite.addTest(makeSuite(TestLogOut))
     return test_suite
 
-
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(full_suite())
